e99f712aa56b74e6398fd54e3cc02df6f9678a97
Agrim Asthana <dev@agrimasthana.com>
2019-02-02 15:27:18 -0600

    Added readme and cors

---
+++ README.md
@@ -0,0 +1,4 @@
+## i14y backend
+<hr>
+
+This is the backend for the #lifescihack2019 . Front end will be coming shortly.
--- index.js
+++ index.js
@@ -19,6 +19,12 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/patient', patient);
 app.use('/ehr', ehr);
 
